9c00c8f7e80da810e827cffecba245c05aab31ee
Adrian Hossen <adrianhossen5@gmail.com>
2022-12-14 18:31:58 -0500

    Initiate Note Create

--- pages/noteCreate.tsx
+++ pages/noteCreate.tsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 const NoteCreate = () => {
   return (
-    <div className="text-center">
-      
-    </div>
+    // <div className="flex-col text-center py-24">
+    //   <div
+    //     className="mx-auto max-w-sm text-xl text-left xs:max-w-sm sm:max-w-sm 
+    //     md:max-w-sm lg:max-w-md xl:max-w-lg 2xl:max-w-lg"
+    //   >
+    //     <div className="mb-4 text-lg font-bold">Title: </div>
+        
+    //     <p className="mb-4 text-lg font-bold">Note: </p>
+    //   </div>
+    // </div>
+    <form action="/api/form" method="post">
+    <label htmlFor="first">First Name</label>
+    <input type="text" id="first" name="first" required />
+
+    <label htmlFor="last">Last Name</label>
+    <input type="text" id="last" name="last" required />
+
+    <button type="submit">Submit</button>
+  </form>
   );
 };
 
