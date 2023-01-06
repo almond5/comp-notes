f8bd9d91eeb246d31025da4fdf3b2436c032f1d6
Adrian Hossen <adrianhossen5@gmail.com>
2023-01-06 13:38:36 -0500

    working on modal

---
+++ components/editModal.tsx
@@ -0,0 +1,91 @@
+import { useSession } from 'next-auth/react';
+import { FcCheckmark } from 'react-icons/fc';
+import { FcCancel } from 'react-icons/fc';
+import React from 'react';
+
+const EditModal = (props: {
+  setEditView: any;
+  notes: any;
+  setDeleted: any;
+  count: any;
+  setCount: any;
+}) => {
+  const { data: session } = useSession();
+  const note = props.notes;
+  const title = note.title;
+  const body = note.body;
+  const date = note.date;
+
+  const timeout = (delay: number) => {
+    return new Promise((res) => setTimeout(res, delay));
+  };
+
+  const handleClose = () => {
+    props.setEditView(false);
+  };
+
+  const handleDelete = async () => {
+    const user = session?.user;
+    const note = { title, body, user, date };
+    await deleteNote(note);
+    props.setDeleted(true);
+    props.setCount(props.count + 1);
+    props.setEditView(false);
+    await timeout(1000);
+  };
+
+  const deleteNote = async (note: {
+    title: string;
+    body: string;
+    date: string;
+    user:
+      | {
+          name?: string | null | undefined;
+          email?: string | null | undefined;
+          image?: string | null | undefined;
+        }
+      | undefined;
+  }) => {
+    const response = await fetch('/api/noteDelete', {
+      method: 'POST',
+      body: JSON.stringify(note),
+    });
+
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
+    <div
+      className="fixed inset-0 flex flex-col justify-center items-center bg-gray-600 
+      bg-opacity-50 z-50 "
+    >
+      <div
+        className="h-[12rem] outline bg-stone-50
+        p-8 rounded-lg"
+      >
+        <div className="text-center break-all text-xl">
+          This note will be edited:
+          <div className="py-4 font-bold break-all">{note.title}</div>
+        </div>
+        <div className='flex justify-evenly'>
+          <button
+            onClick={() => handleClose()}
+            className="px-2 rounded-full py-0.5 font-bold transition hover:bg-gray-300 
+        hover:text-gray-800 text-Lg"
+          >
+            <FcCancel style={{ fontSize: '40px' }} />
+          </button>
+          <button
+            className="px-2 rounded-full py-0.5 font-bold transition hover:bg-gray-300 
+          hover:text-gray-800 text-Lg"
+          >
+            <FcCheckmark style={{ fontSize: '40px' }} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EditModal;
